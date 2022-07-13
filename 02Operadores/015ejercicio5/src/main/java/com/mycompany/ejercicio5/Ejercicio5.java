3219619d40ff129d1e4fb431cb97146a2f5204e5
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-13 17:01:25 -0500

    ejercicio 15

--- 02Operadores/015ejercicio5/src/main/java/com/mycompany/ejercicio5/Ejercicio5.java
+++ 02Operadores/015ejercicio5/src/main/java/com/mycompany/ejercicio5/Ejercicio5.java
@@ -2,10 +2,6 @@ package com.mycompany.ejercicio5;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alemr
- */
 public class Ejercicio5 {
 
     /*
@@ -34,7 +30,7 @@ public class Ejercicio5 {
         
         System.out.println("ingrese la calificacion de su primer examen");
         exaf = entrada.nextFloat();
-        
+        //(1 * 0.10) + (4 * 0.25) + (5 * 0.25) + (5 * 0.40)
         calif = (part * 0.10f) + (exa1 * 0.25f) + (exa2 * 0.25f) + (exaf * 0.40f) ;
         System.out.println("La calificacion final es: "+calif);
               
