65f30531e882f8ed649e5bd3928d84a985e2de67
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-12 19:55:45 -0500

    ejerciio 3

--- 02Operadores/013Ejercicio3/src/main/java/com/mycompany/ejercicio3/Ejercicio3.java
+++ 02Operadores/013Ejercicio3/src/main/java/com/mycompany/ejercicio3/Ejercicio3.java
@@ -2,10 +2,7 @@ package com.mycompany.ejercicio3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alemr
- */
+
 public class Ejercicio3 {
     /*
         Guillermo tiene N dinero, Luis tiene la mitad de Guillermo
@@ -14,14 +11,15 @@ public class Ejercicio3 {
     */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        //declaro todas las varibales necesarias
         float gui, luis, juan, cant;
         System.out.println("Ingresa la cantidad que tiene Guillermo");
         gui = entrada.nextFloat();
-        
+        //aca obtengo la mitad
         luis = gui / 2;
-        
+        //obtengo lo de juan
         juan = (luis + gui) / 2;
-        
+        //al final sumo todo
         cant = gui + luis + juan;
         
         System.out.println("Entre los 3 poseen $"+cant);
