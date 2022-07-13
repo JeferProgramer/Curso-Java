3b5bbbc292ec29379f915d25a2fb9e39dfb3be20
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-12 19:42:42 -0500

    captura de datos grafica en java

--- 01ConceptosBasicos/006EntradaSalidaGrafica/src/main/java/com/mycompany/entradasalidagrafica/EntradaSalidaGrafica.java
+++ 01ConceptosBasicos/006EntradaSalidaGrafica/src/main/java/com/mycompany/entradasalidagrafica/EntradaSalidaGrafica.java
@@ -2,10 +2,7 @@ package com.mycompany.entradasalidagrafica;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
+
 public class EntradaSalidaGrafica {
 
     public static void main(String[] args) {
@@ -13,8 +10,9 @@ public class EntradaSalidaGrafica {
         int edad;
         char letra;
         float decimal;
-        
+        //me guardo en las varibales lo que ingrese el usuario showInputDialog para string de input
         nombre = JOptionPane.showInputDialog("Digite su nombre: ");
+        //todo lo que llegue de joptionpane llega  como una cade de texto por ende cuando necesitamos datos como numeros tenemos que convertirloa un dato que queramos
         edad = Integer.parseInt(JOptionPane.showInputDialog("Digite su edad: "));
         letra = JOptionPane.showInputDialog("Digite una letra: ").charAt(0);
         decimal = Float.parseFloat(JOptionPane.showInputDialog("Digite un numero decimal: "));
