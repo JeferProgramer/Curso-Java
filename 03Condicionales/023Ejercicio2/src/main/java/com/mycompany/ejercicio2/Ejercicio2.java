b7d7d61a894350348694b5b0863e9c1128cb0cf6
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-14 20:05:40 -0500

    ejercicio 2

--- 03Condicionales/023Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
+++ 03Condicionales/023Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -2,10 +2,7 @@ package com.mycompany.ejercicio2;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
+
 public class Ejercicio2 {
 
     /*
@@ -13,27 +10,20 @@ public class Ejercicio2 {
         y retorne cual es el mayor o si son iguales
     */
     public static void main(String[] args) {
-        //Declaracion de variables
+        //Declaro variables 
         float num1, num2;
-        
+
         //Entrada de los numeros
         num1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un numero"));
-        num2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un numero"));
-    
+        num2 = float.parseFloat(JOptionPane.showInputDialog("Ingrese un numero"));
+
         //Condicion principal
-        if (num1 > num2)
-        {
-            JOptionPane.showMessageDialog(null, num1+" es el mayor");
-        }
-        //Condicion secundaria
-        else if (num2 > num1    )
-        {
-            JOptionPane.showMessageDialog(null, num2+" es el mayor");
-        }
-        //Opcion por defecto
-        else
-        {
-            JOptionPane.showMessageDialog(null,"Los numeros son iguales");
+        if(num1 > num2){
+            JOptionPane.showMessageDialog(null, num1 + "es el mayor")
+        }else if(num2 > num1){
+            JOptionPane.showMessageDialog(null, num2+"es el mayor");
+        }else{
+            JOptionPane.showMessageDialog(null,"Los numeros son iguales")
         }
     }
     
