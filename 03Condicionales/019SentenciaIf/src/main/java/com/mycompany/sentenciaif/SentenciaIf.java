6679007b0ef1fb29982381e8c57da351166015f9
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-14 19:21:20 -0500

    empezando con el if

--- 03Condicionales/019SentenciaIf/src/main/java/com/mycompany/sentenciaif/SentenciaIf.java
+++ 03Condicionales/019SentenciaIf/src/main/java/com/mycompany/sentenciaif/SentenciaIf.java
@@ -2,25 +2,30 @@ package com.mycompany.sentenciaif;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
+
 public class SentenciaIf 
 {
     public static void main(String[] args) 
     {
+        // int numero, dato = 5;
+        
+        // numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        
+        // if (numero == dato)
+        // {
+        //     JOptionPane.showMessageDialog(null, "El numero es 5");
+        // }
+        // else
+        // {
+        //     JOptionPane.showConfirmDialog(null, "El numero es diferente de 5");
+        // }
         int numero, dato = 5;
-        
+        //le digo usuario que ingrese un numero
         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
-        
-        if (numero == dato)
-        {
-            JOptionPane.showMessageDialog(null, "El numero es 5");
-        }
-        else
-        {
-            JOptionPane.showConfirmDialog(null, "El numero es diferente de 5");
+        if(numero == dato){
+            JOptionPane.showMessageDialog(null,"El numero es 5");
+        }else{
+            JOptionPane.showConfirmDialog(null,"El numero es diferente de 5")
         }
     }
     
