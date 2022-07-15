0f208a3e10dec95ac95cc9ec148915259b0ba26b
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-14 20:25:29 -0500

    ejercicio 7

--- 03Condicionales/028Ejercicio7/src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
+++ 03Condicionales/028Ejercicio7/src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
@@ -2,18 +2,13 @@ package com.mycompany.ejercicio7;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
 public class Ejercicio7 {
 
     //Crea un programa que ordene 3 numeros de mayor a menos ingresados por el usuario
     public static void main(String[] args) {
+        //declaro variables 
         float numero1, numero2, numero3, memoria;
-        
-
-        
+        //capturo datos y convierto
         numero1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un numero"));
         numero2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un numero"));
         numero3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un numero"));        
@@ -43,58 +38,6 @@ public class Ejercicio7 {
             JOptionPane.showMessageDialog(null, "Orden: "+numero3+", "+numero2+", "+numero1);
         }
         
-        
-        /*
-            if(numero1 > numero2 && numero1 > numero3)
-            {
-                if(numero2 > numero3)
-                {
-                    numero1 = numero1;
-                    numero2 = numero2;
-                    numero3 = numero3;
-                }
-                else
-                {
-                    memoria = numero2;
-                    numero2 = numero3;
-                    numero3 = memoria;
-                }
-            }
-            else if(numero2 > numero1 && numero2 > numero3)
-            {
-                if(numero1 > numero3)
-                {
-                    memoria = numero1;
-                    numero1 = numero2;
-                    numero2 = memoria;
-                    numero3 = numero3;
-                }
-                else
-                {
-                    memoria = numero1;
-                    numero1 = numero2;
-                    numero2 = numero3;
-                    numero3 = memoria;
-                }
-            }
-            else if(numero3 > numero1 && numero3 > numero2)
-            {
-                if(numero1 > numero2)
-                {
-                    memoria = numero1;
-                    numero1 = numero3;
-                    numero3 = numero2;
-                    numero2 = memoria;
-                }
-                else
-                {
-                    memoria = numero1;
-                    numero1 = numero3;
-                    numero3 = memoria;
-                }
-            }
-            JOptionPane.showMessageDialog(null, "Del mayor al menor son: "+numero1+", "+numero2+", "+numero3);      
-        */
     }
     
 }
