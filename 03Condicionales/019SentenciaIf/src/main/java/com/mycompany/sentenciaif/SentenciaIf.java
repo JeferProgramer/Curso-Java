6cadb9cac7109eefa9041111a505cc1af6e52134
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-16 12:52:36 -0500

    sentencia if correjido

--- 03Condicionales/019SentenciaIf/src/main/java/com/mycompany/sentenciaif/SentenciaIf.java
+++ 03Condicionales/019SentenciaIf/src/main/java/com/mycompany/sentenciaif/SentenciaIf.java
@@ -7,25 +7,13 @@ public class SentenciaIf
 {
     public static void main(String[] args) 
     {
-        // int numero, dato = 5;
-        
-        // numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
-        
-        // if (numero == dato)
-        // {
-        //     JOptionPane.showMessageDialog(null, "El numero es 5");
-        // }
-        // else
-        // {
-        //     JOptionPane.showConfirmDialog(null, "El numero es diferente de 5");
-        // }
         int numero, dato = 5;
         //le digo usuario que ingrese un numero
         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
         if(numero == dato){
             JOptionPane.showMessageDialog(null,"El numero es 5");
         }else{
-            JOptionPane.showConfirmDialog(null,"El numero es diferente de 5")
+            JOptionPane.showConfirmDialog(null,"El numero es diferente de 5");
         }
     }
     
