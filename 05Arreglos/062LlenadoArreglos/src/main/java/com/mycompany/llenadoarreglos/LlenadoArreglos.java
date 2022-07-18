2206083a086dd7a3409c6fb68e0c4eb54698de1f
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 14:05:51 -0500

    llenando arreglos en java

--- 05Arreglos/062LlenadoArreglos/src/main/java/com/mycompany/llenadoarreglos/LlenadoArreglos.java
+++ 05Arreglos/062LlenadoArreglos/src/main/java/com/mycompany/llenadoarreglos/LlenadoArreglos.java
@@ -3,25 +3,23 @@ package com.mycompany.llenadoarreglos;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
+
 public class LlenadoArreglos {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         //Variable del numero total de elementos
         int numeroElementos;
-        numeroElementos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de elementos del arreglos: "));
+        System.out.println("Ingrese la cantidad de elementos del arreglos: ");
+        numeroElementos = entrada.nextInt();
         
         //Arreglo de puras letras
         char[] letras = new char[numeroElementos];
         
-        System.out.println("Digite los elemeentos de su arreglo: ");
+        System.out.println("Digite los elementos de su arreglo: ");
         for (int i = 0; i < numeroElementos; i++)   //Bucle para ir llenando el arreglo
         {
-            System.out.print("Digite un caracter");
+            System.out.println("Digite un caracter");
             letras[i] = entrada.next().charAt(0);   //Elementos a llenar
         }
         
