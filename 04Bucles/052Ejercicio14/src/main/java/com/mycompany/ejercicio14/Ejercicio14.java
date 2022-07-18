e7d2e39a1d1092ad7ae0a9cae0d4de8ff4851e47
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 10:25:21 -0500

    ejercicio 14 bucles

--- 04Bucles/052Ejercicio14/src/main/java/com/mycompany/ejercicio14/Ejercicio14.java
+++ 04Bucles/052Ejercicio14/src/main/java/com/mycompany/ejercicio14/Ejercicio14.java
@@ -2,10 +2,6 @@ package com.mycompany.ejercicio14;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
 public class Ejercicio14 {
 
     /*
@@ -14,18 +10,24 @@ public class Ejercicio14 {
         son menores a $1000
     */
     public static void main(String[] args) {
+        //declarar variables 
         float i, sueldos, contador = 0, suma = 0;
-        
+        //recorremos y solicitamos los 10 sueldos que necesitamos
         for (i = 1; i <= 10; i++) 
         {
+            //me guardo el sueldo que escribe
             sueldos = Float.parseFloat(JOptionPane.showInputDialog("Ingresa tu sueldo"));
+            //si el numero es menor a 1000 incremento el contador de menor a 1000
             if (sueldos <1000)
             {
                 contador++;
             }
+            //hago la suma con todos los sueldos uno por uno por cada recorrido
             suma = suma + sueldos;
         }
+        //mostrar la suma 
         System.out.println("La suma de todos los sueldos es: "+suma);
+        //mostrar el contador
         System.out.println("El numero de sueldos menores a mil es: "+contador);
     }
     
