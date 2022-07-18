da845ebabe2f5295c249b2dc6956c4f08605d8bd
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 14:00:44 -0500

    empezando con arreglos en java

--- 05Arreglos/061Arreglos/src/main/java/com/mycompany/arreglos/Arreglos.java
+++ 05Arreglos/061Arreglos/src/main/java/com/mycompany/arreglos/Arreglos.java
@@ -1,9 +1,5 @@
 package com.mycompany.arreglos;
 
-/**
- *
- * @author alemr
- */
 public class Arreglos {
 
     /*
@@ -12,15 +8,13 @@ public class Arreglos {
     */
     public static void main(String[] args) {
         
-        //Ejemplos de arrays con sus tipos de datos
-        int[] edades  = new int[2];
-        long[] factoriales  = new long[4];
-        float[] estaturas  = new float[3];
-        double[] imaginarios  = new double[5];
-        boolean[] estados  = new boolean[2];
-        char[] sexos  = new char[2];
-        String[] nombres  = new String[3];
-        
+        int[] edades = new int[2];
+        long[] factoriales = new long[4];
+        float[] estaturas = new float[3];
+        double[] imaginarios = new double[5];
+        boolean[] estados = new boolean[2];
+        char[] sexos = new char[2];
+        String[] nombres = new String[3];
     }
     
 }
