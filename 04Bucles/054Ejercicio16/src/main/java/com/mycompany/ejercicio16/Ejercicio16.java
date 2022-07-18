40cc700754962d153640666c2af703864ab672fd
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 11:30:33 -0500

    ejercicio tabla de multiplicat java

--- 04Bucles/054Ejercicio16/src/main/java/com/mycompany/ejercicio16/Ejercicio16.java
+++ 04Bucles/054Ejercicio16/src/main/java/com/mycompany/ejercicio16/Ejercicio16.java
@@ -2,10 +2,7 @@ package com.mycompany.ejercicio16;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
+
 public class Ejercicio16 {
 
     /*
@@ -14,10 +11,12 @@ public class Ejercicio16 {
         en un rango del 0 al 10
     */
     public static void main(String[] args) {
+        //declaro variables
         int numero, i,j, resultado;
-        
+        //pedimos que ingrese la tabla de multiplicar
         numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero"));
         System.out.println("Tabla de multiplicar del: "+numero);
+        //empiezo a recorrer hasta el numero 10
         for(i = 0; i<=10; i++)
         {
             System.out.println(numero+" x "+i+" = "+numero*i);
