b8c9dae89d5e817632bf9ed2ebc396937f880a6d
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 14:47:23 -0500

    ejercicio 2

--- 05Arreglos/065Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
+++ 05Arreglos/065Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -2,10 +2,6 @@ package com.mycompany.ejercicio2;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alemr
- */
 public class Ejercicio2 {
 
     /*
@@ -15,14 +11,15 @@ public class Ejercicio2 {
     */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        //declaro el array de 5 posiciones
         int[] numeros = new int[5];
-        
+        //en el for recorro las 5 veces y voy llenando el array
         for (int i = 0; i < 5; i++)
         {
             System.out.println("Ingresa un numero:");
             numeros[i] = entrada.nextInt();
         }
-        
+        //hago el reverso entonces con i decremento e i es igual a 4
         for (int i = 4; i >= 0; i--)
         {
             System.out.println("Numero: "+numeros[i]);
