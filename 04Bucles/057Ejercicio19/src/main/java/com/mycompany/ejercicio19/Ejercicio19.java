c917dc3c6a825b3a6ec6482b0467bfc4fd969d96
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 13:04:46 -0500

    ejercicio 19 java

--- 04Bucles/057Ejercicio19/src/main/java/com/mycompany/ejercicio19/Ejercicio19.java
+++ 04Bucles/057Ejercicio19/src/main/java/com/mycompany/ejercicio19/Ejercicio19.java
@@ -2,10 +2,6 @@ package com.mycompany.ejercicio19;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
 public class Ejercicio19 {
 
     /*
@@ -14,24 +10,29 @@ public class Ejercicio19 {
         aprobaron, son condicionados y suspensos
     */
     public static void main(String[] args) {
+        //declaro variables
         int i, calificaciones;
-        
+        //recorro los 6 estudiantes
         for(i = 1; i <= 6; i++)
         {
+            //capturo las calificaciones
             calificaciones = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la calificacion final: "));
-            
+            //si es mayor a 6 esta a probado
             if(calificaciones >=6)
             {
                 System.out.println("Aprobado");
             }
+            //si es igual a 4 es condicionado
             else if(calificaciones == 4)
             {
                 System.out.println("Condicionado");
             }
+            //si es mayor a cero y menor a 5 o igual y difrente de 4 suspenso
             else if(calificaciones>=0  && calificaciones <=5 && calificaciones!=4)
             {
                 System.out.println("Suspenso");
             }
+            //si no es erronea
             else
             {
                 System.out.println("Calificacion erronea");
