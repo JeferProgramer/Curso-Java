c75c07393d69887f1e40f1fff8bdab51a5c47885
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-18 13:07:40 -0500

    ejercicio 20 ciclos

--- 04Bucles/058Ejercicio20/src/main/java/com/mycompany/ejercicio20/Ejercicio20.java
+++ 04Bucles/058Ejercicio20/src/main/java/com/mycompany/ejercicio20/Ejercicio20.java
@@ -2,10 +2,6 @@ package com.mycompany.ejercicio20;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author alemr
- */
 public class Ejercicio20 {
 
     /*
@@ -14,21 +10,28 @@ public class Ejercicio20 {
         sueldo maximo
     */
     public static void main(String[] args) {
+        //declaro variables
         int i, sueldo, numeroSueldos, sueldoMaximo;
-        
+        //numero de sueldos que quiere el usuario
         numeroSueldos = Integer.parseInt(JOptionPane.showInputDialog("Ingresa cuantos sueldos vas a registrar"));
+        //si es mayor 0 entro aca
         if(numeroSueldos > 0)
         {
+            //ingreso el primerr sueldo y se lo asigno a sueldo maximo
             sueldo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el monto del sueldo: "));
             sueldoMaximo = sueldo;
+            //recorro los sueldos que solicito el usuario
             for(i = 1; i<numeroSueldos; i++)
             {
+                //dentro del for le pido que ingrese el sueldo
                 sueldo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el monto del sueldo: "));
+                //si es mayor mi sueldo ultimo ingresado al sueldo maximo le asigno ese valor de ese seuldo a sueldo maximo
                 if(sueldo > sueldoMaximo)
                 {
                     sueldoMaximo = sueldo;
                 }
             }
+            //imprimo el sueldo
             System.out.println("El sueldo maximo es: $"+sueldoMaximo);
         }
         else
