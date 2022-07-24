f4eb0df661b96b8bfe04a505ddcaa961385a54f2
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-23 20:16:20 -0500

    busqueda secuencial lista

--- 07Busquedas/083Secuencial/src/main/java/com/mycompany/secuencial/Secuencial.java
+++ 07Busquedas/083Secuencial/src/main/java/com/mycompany/secuencial/Secuencial.java
@@ -2,16 +2,13 @@ package com.mycompany.secuencial;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alemr
- */
 public class Secuencial {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         Scanner entrada = new Scanner(System.in);
         int[] arreglo = {4,1,5,2,3};
         int dato;
