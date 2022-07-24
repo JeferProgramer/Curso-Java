35b7e5d0e37cea37bba3eb1c24589566a74f6339
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-24 18:51:56 -0500

    polimorfismo en clase vehiculo

---
+++ POO/Poliformismo/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args){
+        Vehiculo misVehiculos[] = new Vehiculo[2];
+        misVehiculos[0] = new Vehiculo("gh4758", "ferrari", "a87");
+        misVehiculos[1] = new VehiculoTurismo(4, "78hj", "Toyato", "a87");
+        
+    }
+}
