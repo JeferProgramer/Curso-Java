c4b8a02b2f94071bd8deaf47440a59655071b501
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-24 18:55:28 -0500

    ejecutando en main

--- POO/Poliformismo/Principal.java
+++ POO/Poliformismo/Principal.java
@@ -3,6 +3,5 @@ public class Principal {
         Vehiculo misVehiculos[] = new Vehiculo[2];
         misVehiculos[0] = new Vehiculo("gh4758", "ferrari", "a87");
         misVehiculos[1] = new VehiculoTurismo(4, "78hj", "Toyato", "a87");
-        
     }
 }
