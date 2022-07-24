16050c506d3957530408afdcc1ba13aacb618d4b
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-24 16:58:36 -0500

    metodo implementado en planta

--- POO/Clases y metodos abstractos/Planta.java
+++ POO/Clases y metodos abstractos/Planta.java
@@ -1,3 +1,7 @@
 public class Planta extends serVivo {
-    
+    //para indicar que vamos a utilizar un metodo abstracto 
+    @Override
+    public void alimentarse(){
+        System.out.println("La planta se alimenta trave z de fotosintesis");
+    }
 }
