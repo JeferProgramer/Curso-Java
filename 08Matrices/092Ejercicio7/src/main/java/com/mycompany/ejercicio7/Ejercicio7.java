7dcbc2e8aea9add0b5bbf5d2cc188fa766d1a454
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-23 21:09:36 -0500

    poniendo mi ejercicio

--- 08Matrices/092Ejercicio7/src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
+++ 08Matrices/092Ejercicio7/src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
@@ -13,29 +13,29 @@ public class Ejercicio7 {
     */
     public static void main(String[] args) {
         int[][] matriz = new int[5][5];
-        
-        //Se rellena la matriz
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (i == 0 || i == 4) { //Controla a las columnas
-                    matriz[i][j] = 1;
-                }
-                else if (j == 0 || j == 4) {    //Controla las filas
-                    matriz[i][j] = 1;
-                }
-                else{
-                    matriz[i][j] = 0;
-                }
-            }
-        }
-        
-        System.out.println("La matriz es: ");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(matriz[i][j]+" ");
-            }
-            System.out.println("");
-        }
+       
+       //vamos a rellenar el array
+       for(int i = 0; i < 5; i++){
+           for(int j = 0; j < 5; j++){
+               if(i == 0 || i == 4){
+                   //contro a las columnas
+                   matriz[i][j] = 1;
+               }else if(j == 0 || j == 4){
+                   //contro las filas
+                   matriz[i][j] = 1;
+               }else{
+                   matriz[i][j] = 0;
+               }
+           }
+       }
+       
+       System.out.println("La matriz es : ");
+       for(int i = 0; i < 5; i++){
+           for(int j = 0; j < 5; j++){
+               System.out.print(matriz[i][j] + " ");
+           }
+           System.out.println("");
+       }
     }
     
 }
