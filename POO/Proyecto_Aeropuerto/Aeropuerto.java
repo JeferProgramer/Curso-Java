34b1e3ad0940cc6b98989104ed224e716f9be04e
Jeferson David Cañon Melo <97969138+JeferProgramer@users.noreply.github.com>
2022-08-10 19:37:17 -0500

    Update Aeropuerto.java
    declarar variables en aeropuerto

--- POO/Proyecto_Aeropuerto/Aeropuerto.java
+++ POO/Proyecto_Aeropuerto/Aeropuerto.java
@@ -1,1 +1,10 @@
+public class Aeropuerto{
+  private String nombre;
+  private String cuidad;
+  private String pais;
+  private Compañia listaCompañias[] = new Compañia[10];
+  private int numCompañia;
+  
+  
+}
 
