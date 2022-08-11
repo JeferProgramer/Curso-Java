b7f00d43f64aa63b89b7e731e4ae65db2f9a092f
Jeferson David Cañon Melo <97969138+JeferProgramer@users.noreply.github.com>
2022-08-11 09:42:43 -0500

    Update AeropuertoPublico.java
    segundo constructor

--- POO/Proyecto_Aeropuerto/AeropuertoPublico.java
+++ POO/Proyecto_Aeropuerto/AeropuertoPublico.java
@@ -4,4 +4,8 @@ public class AeropuertoPublico{
   public AeropuertoPublico(String nombre, String cuidad, String pais){
     super(nombre, cuidad, pais);
   }
+  public AeropuertoPublico.(double subvencion,String nombre, String, cuidad, String pais, Compañia c []){
+    super(nombre, cuidad, pais,c);
+    this.subvencion = subvencion;
+  }
 }
