96cd68a54bc3d78be7918bb7c83e269bdf57d98a
Jeferson David Cañon Melo <97969138+JeferProgramer@users.noreply.github.com>
2022-08-11 10:03:20 -0500

    Update Principal.java
    variables en main

--- POO/Proyecto_Aeropuerto/Principal.java
+++ POO/Proyecto_Aeropuerto/Principal.java
@@ -1,1 +1,5 @@
-
+public class Principal{
+  static Scanner entrada = new Scanner(System.in);
+  final static int num = 4 //Numero de aeropuertos;
+  static Aeropuerto aeropuertos[] = new Aeropuerto[num];
+}
