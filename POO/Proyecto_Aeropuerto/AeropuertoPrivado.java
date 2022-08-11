5040f37936b770a6e07a8799e139ea62f7321fc8
Jeferson David Cañon Melo <97969138+JeferProgramer@users.noreply.github.com>
2022-08-11 08:44:47 -0500

    Update AeropuertoPrivado.java
    aeropuerto constructor

--- POO/Proyecto_Aeropuerto/AeropuertoPrivado.java
+++ POO/Proyecto_Aeropuerto/AeropuertoPrivado.java
@@ -1,1 +1,10 @@
-
+public class AeropuertoPrivado extends Aeropuerto{
+  private String listaEmpresas[] = new String[10];
+  private int numEmpresa;
+  
+  public AeropuertoPrivado(String nombre, String cuidad, String pais){
+    super(nombre, cuidad, pais);
+  }
+  
+  
+}
