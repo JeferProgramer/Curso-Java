e05b0f5204c22f5c0a639623334123a2fb7a62c2
JeferProgramer <jefer.talento.11@gmail.com>
2022-08-12 15:18:39 -0500

    metodo para leer archivo en java

--- 10Exepciones/Excepciones.java
+++ 10Exepciones/Excepciones.java
@@ -4,5 +4,10 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Excepciones{
+    public void leerArchivo() throws FileNotFoundException{
+        File archivo = new File("CC:/Documents/texto.txt");
+        FileReader fr = new FileReader(archivo);
+        System.out.println(fr);
+    }
 
 }
