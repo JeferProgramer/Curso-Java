12dd7bf53dae50a29a5009aa78a892101ddfee68
JeferProgramer <jefer.talento.11@gmail.com>
2022-08-12 20:09:41 -0500

    agregando el bucle do while para el numero

--- 10Exepciones/PruebaExcepciones.java
+++ 10Exepciones/PruebaExcepciones.java
@@ -4,6 +4,11 @@ public class PruebaExcepciones {
     private Scanner entrada;
     public void introducirNumeros(){
         entrada = new Scanner(System.in);
+
+        do{
+            System.out.println("Digite un numero:");
+            numero = entrada.nextInt();
+        }while(numero != 0);
     }
     public static void main(String[] args){
 
