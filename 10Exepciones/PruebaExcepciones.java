b82c3f239dd1e1011fdd1351b57e6e5ce464618f
JeferProgramer <jefer.talento.11@gmail.com>
2022-08-12 20:00:18 -0500

    trayendome scanner

--- 10Exepciones/PruebaExcepciones.java
+++ 10Exepciones/PruebaExcepciones.java
@@ -3,7 +3,7 @@ public class PruebaExcepciones {
     private int numero;
     private Scanner entrada;
     public void introducirNumeros(){
-        
+        entrada = new Scanner(System.in);
     }
     public static void main(String[] args){
 
