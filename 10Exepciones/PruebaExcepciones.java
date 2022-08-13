e9b1496583e5d00e3cf7bb18a727e0c5c3adccd4
JeferProgramer <jefer.talento.11@gmail.com>
2022-08-12 19:59:23 -0500

    empezando con el metodo introducir nombre

--- 10Exepciones/PruebaExcepciones.java
+++ 10Exepciones/PruebaExcepciones.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class PruebaExcepciones {
     private int numero;
     private Scanner entrada;
+    public void introducirNumeros(){
+        
+    }
     public static void main(String[] args){
 
     }
